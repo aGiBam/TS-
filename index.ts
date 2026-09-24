205f589b70844d9138d7cacaea93c7994b16ff85
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-24 14:20:59 +0300

    chore: remove commented-out functions and streamline PostGateKeeper interface in index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,12 @@
-//Type Inference 
-// const add = (a: number, b: number): number => {
-//     return  a + b;
-// };
-//Type Inference 
-// const joinStrings = (a: string, b: string): string => {
-//     return  a + b;
-// };
-
-function add(a:number , b:number): number {
-    return a + b;
-};
-
-const joinString = function(a: string, b: string): string {
-    return a + b;
+interface PostGateKeeper {
+  title: string;
+  daysOld: number;
+  published: boolean;
 }
+//interface -> describe the structure of an object, defining the properties and their types.
+
+const Post: PostGateKeeper = {
+    title: "Understanding TypeScript Interfaces",
+    daysOld: 5,
+    published: true
+};
