1fa77f26db92406d2537dc65fb0cca039c7edaa1
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-24 13:11:19 +0300

    chore: add TypeScript type annotations and example variables

---
+++ index.ts
@@ -0,0 +1,11 @@
+// Type Annotations in TypeScript are a way to explicitly specify the types of variables, function parameters, and return values. This helps catch errors at compile time and improves code readability. In the provided code snippet, various type annotations are used to define the types of different variables.
+// console.log('Hi There'); //npm start
+const myName: string = 'Mohammed Agiba';
+console.log(myName);
+const exType: any = undefined;
+const ten: number = 10;
+const decimal: number = 1.0002
+const negative: number = -1000;
+const isTrue: boolean = true;
+const nothingHere: null = null;
+const noDefinition: undefined = undefined;
