3b6a7aa03c7e20f20d983981c3bb48aa9c8eb3ad
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-24 15:33:11 +0300

    chore: implement Car class and update index.ts to create an instance

--- index.ts
+++ index.ts
@@ -1,10 +1,3 @@
-const Component = (target: any) => { 
-console.log(target);
+import { Car }  from './Car';
 
-};
-
-@Component
-class Car {}
-
-//Component doesn't get called a second time because decorators are only called once when the class is defined.
-new Car();
+const myCar = new Car();
