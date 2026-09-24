80e183de72b81b1731b18005e8075f1baa2bc00a
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-24 15:50:08 +0300

    chore: implement Driveable interface and car class with driving functionality

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-interface
+interface Driveable {
+    speed: number;
+    drive(): string;
+}
+
+class car implements Driveable {
+    speed = 10;
+
+    drive() {
+        return `I am driving at ${this.speed}`;
+        
+    }
+}
+
+const myCar = new car();
+
+const startDriving = (vehicle: Driveable) => {
+    vehicle.drive();
+};
+
+startDriving(myCar);
