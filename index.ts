3959c790ed14043916e472aa15cb02234ef7d4a7
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-24 15:11:32 +0300

    chore: refine Car class by utilizing public/private modifiers and removing commented-out code

--- index.ts
+++ index.ts
@@ -1,18 +1,37 @@
+//public / private Modifiers 
 class Car {
-    color: string;
-    year: number;
+    // public color: string;
+    // private year: number;
 
-    constructor(color: string, year: number) {
+    constructor(public color: string, private year: number) {
         this.color = color;
         this.year = year;
     }
     // color = 'blue';
     // year = 2026;
-    drive() {
-        console.log(`The ${this.color} car from ${this.year} is driving.`);
+    public drive() {
+        // console.log(`The ${this.color} car from ${this.year} is driving.`);
+        // console.log(this.year);
+        this.putInGear();
+        this.pressPedal();
+        this.turnWheel();
+    }
+
+    private putInGear() {
+        
+    }
+    
+    private pressPedal() {
+
+    }
+    
+    private turnWheel() {
+
     }
 }
 
 const myCar = new Car('Silver', 2000);
 myCar.drive();
-console.log('color: '+ myCar.color+ ', year: '+ myCar.year);
+// console.log('color: '+ myCar.color+ ', year: '+ myCar.year);
+
+// myCar.putInGear(); // Error: Property 'putInGear' is private and only accessible within class 'Car'.
